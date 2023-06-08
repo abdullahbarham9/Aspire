174064db128489d7de4b34f61fbbdd9fafc0821d
abdullah barham <abdullah.lotfe.barham11@gmail.com>
2023-06-08 11:52:28 +0300

    new task 2

--- Application.java
+++ Application.java
@@ -1,26 +1,20 @@
-
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Application {
     public static void main(String[] args) {
-        int[] numberArray = {1, 2, 3, 4, 5};
-        int[] reversedArray = reverseArray(numberArray);
 
+            int[] numberArray = {1, 2, 3, 4, 5};
+            int[] reversedArray = reverseArray(numberArray);
 
-        for (int i = 0; i < reversedArray.length; i++) {
-            System.out.print(reversedArray[i]);
+            Arrays.stream(reversedArray).forEach(System.out::print);
         }
 
-    }
-
-    public static int[] reverseArray(int[] array) {
-        int[] reversedArray = new int[array.length];
-
-        for (int i = 0; i < array.length; i++) {
-            reversedArray[i] = array[array.length - i - 1];
+        public static int[] reverseArray(int[] array) {
+            return IntStream.range(0, array.length)
+                    .map(i -> array[array.length - i - 1])
+                    .toArray();
         }
-
-        return reversedArray;
-    }
 
 
 }
